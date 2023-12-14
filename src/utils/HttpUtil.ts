79ead43059548a04563f4451ce4a9d73b7224afd
liuqixing <liuqixing@dekuan.org>
2023-12-14 10:54:01 +0800

    1.0.55, Added HttpUtil.test.ts

---
+++ src/utils/HttpUtil.ts
@@ -0,0 +1,166 @@
+import _ from "lodash";
+import axios, { AxiosRequestConfig, AxiosResponse, AxiosProxyConfig, AxiosHeaders } from 'axios';
+
+/**
+ * 	models
+ */
+export enum HttpUtilMethods
+{
+	GET = "GET",
+	HEAD = "HEAD",
+	POST = "POST",
+	PUT = "PUT",
+	DELETE = "DELETE",
+	CONNECT = "CONNECT",
+	OPTIONS = "OPTIONS",
+	TRACE = "TRACE",
+	PATCH = "PATCH"
+}
+export type HttpUtilMilliseconds = number;
+
+export const HttpUtilDefaultTimeout : HttpUtilMilliseconds	= 5 * 60 * 1000;	//	5 minutes
+export const HttpUtilDefaultAccept : string	= 'application/json';
+
+export interface HttpUtilProxy extends AxiosProxyConfig
+{
+}
+
+export interface HttpUtilHeaderObject
+{
+	[ key: string ] : string | string[] | number | boolean | null;
+}
+export interface HttpUtilOptions
+{
+	url : string,				//	url
+	method ?: HttpUtilMethods;		//	method
+	timeout? : HttpUtilMilliseconds;	//	timeout in milliseconds
+	data? : object;				//	post data
+	headers? : HttpUtilHeaderObject;	//	http headers
+	proxy?: HttpUtilProxy;			//	proxy
+}
+
+export interface HttpUtilResponse
+{
+	status ?: number;
+	statusText ?: string;
+	headers ?: any;
+	data ?: any;
+}
+
+/**
+ * 	@class HttpUtil
+ */
+export class HttpUtil
+{
+	public static isValidMethods( value: any ) : boolean
+	{
+		return Object.values( HttpUtilMethods ).includes( value as HttpUtilMethods );
+	}
+
+	public static isValidUrl( value : any ) : boolean
+	{
+		const urlRegex = /^(http|https):\/\/[^ "]+$/;
+		return _.isString( value ) && ! _.isEmpty( value ) && urlRegex.test( value );
+	}
+
+	public static isValidTimeout( value : any ) : boolean
+	{
+		return _.isNumber( value ) && value > 0;
+	}
+
+	public static head( options : HttpUtilOptions ) : Promise< HttpUtilResponse | null >
+	{
+		return this.request( { ...options, method : HttpUtilMethods.HEAD } );
+	}
+
+	public static get( options : HttpUtilOptions ) : Promise< HttpUtilResponse | null >
+	{
+		return this.request( { ...options, method : HttpUtilMethods.GET } );
+	}
+
+	public static post( options : HttpUtilOptions ) : Promise< HttpUtilResponse | null >
+	{
+		return this.request( { ...options, method : HttpUtilMethods.POST } );
+	}
+
+	public static put( options : HttpUtilOptions ) : Promise< HttpUtilResponse | null >
+	{
+		return this.request( { ...options, method : HttpUtilMethods.PUT } );
+	}
+
+	public static patch( options : HttpUtilOptions ) : Promise< HttpUtilResponse | null >
+	{
+		return this.request( { ...options, method : HttpUtilMethods.PATCH } );
+	}
+
+	public static delete( options : HttpUtilOptions ) : Promise< HttpUtilResponse | null >
+	{
+		return this.request( { ...options, method : HttpUtilMethods.DELETE } );
+	}
+
+	public static request( options : HttpUtilOptions ) : Promise< HttpUtilResponse | null >
+	{
+		return new Promise( async ( resolve, reject ) =>
+		{
+			try
+			{
+				if ( ! options )
+				{
+					return reject( `invalid options` );
+				}
+				if ( ! this.isValidMethods( options.method ) )
+				{
+					return reject( `invalid options.method` );
+				}
+				if ( ! this.isValidUrl( options.url ) )
+				{
+					return reject( `invalid options.url` );
+				}
+
+				//	config request options of Axios
+				let axiosConfig: AxiosRequestConfig = {
+					method: options.method,
+					url: options.url,
+					timeout : this.isValidTimeout( options.timeout ) ? options.timeout : HttpUtilDefaultTimeout,
+				};
+				if ( _.isObject( options.headers ) && ! _.isEmpty( options.headers ) )
+				{
+					axiosConfig.headers = options.headers;
+				}
+				if ( _.isObject( options.data ) && ! _.isEmpty( options.data ) )
+				{
+					axiosConfig.data = options.data;
+				}
+				if ( _.isObject( options.proxy ) && ! _.isEmpty( options.proxy ) )
+				{
+					axiosConfig.proxy = options.proxy;
+				}
+
+				try
+				{
+					//	send request
+					const axiosRes: AxiosResponse = await axios( axiosConfig );
+					if ( axiosRes )
+					{
+						const response : HttpUtilResponse = {
+							status : axiosRes.status,
+							statusText : axiosRes.statusText,
+							headers : axiosRes.headers,
+							data : axiosRes.data,
+						};
+						return resolve( response );
+					}
+
+					resolve( null );
+				}
+				catch ( err )
+				{
+					reject( err );
+				}
+			}
+			catch ( err )
+			{
+			}
+		});
+	}
+}
---
+++ test/unit/HttpUtil.test.ts
@@ -0,0 +1,110 @@
+import { expect } from "@jest/globals";
+import { HttpUtil, HttpUtilOptions, HttpUtilResponse } from "../../src/utils/HttpUtil";
+import _ from "lodash";
+import { AxiosBasicCredentials } from "axios";
+
+/**
+ *	unit test
+ */
+describe( "HttpUtil Test", () =>
+{
+	beforeAll( async () =>
+	{
+	} );
+	afterAll( async () =>
+	{
+	} );
+
+	describe( "HttpPost", () =>
+	{
+		it( "should send HTTP Get request", async () =>
+		{
+			const httpOptions : HttpUtilOptions = {
+				url : `https://jsonplaceholder.typicode.com/posts/1`,
+			};
+			const response : HttpUtilResponse | null = await HttpUtil.get( httpOptions );
+			expect( response ).not.toBeNull();
+			expect( response ).toHaveProperty( `status` );
+			expect( response ).toHaveProperty( `statusText` );
+			expect( response ).toHaveProperty( `headers` );
+			expect( response ).toHaveProperty( `data` );
+			if ( response )
+			{
+				expect( response.status ).toBeGreaterThanOrEqual( 200 );
+				expect( response.status ).toBeLessThanOrEqual( 202 );
+				//
+				//	data: {
+				//         userId: 1,
+				//         id: 1,
+				//         title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+				//         body: 'quia et suscipit\n' +
+				//           'suscipit recusandae consequuntur expedita et cum\n' +
+				//           'reprehenderit molestiae ut ut quas totam\n' +
+				//           'nostrum rerum est autem sunt rem eveniet architecto'
+				//       }
+				//
+				expect( response.data ).toHaveProperty( `userId` );
+				expect( response.data ).toHaveProperty( `id` );
+				expect( response.data ).toHaveProperty( `title` );
+				expect( _.isNumber( response.data.userId ) ).toBeTruthy();
+				expect( response.data.userId ).toBeGreaterThan( 0 );
+				expect( _.isNumber( response.data.id ) ).toBeTruthy();
+				expect( response.data.id ).toBeGreaterThan( 0 );
+				expect( _.isString( response.data.title ) ).toBeTruthy();
+			}
+		} );
+
+		it( "should send HTTP Post request", async () =>
+		{
+			const httpOptions : HttpUtilOptions = {
+				url : `https://jsonplaceholder.typicode.com/posts`,
+			};
+			const response : HttpUtilResponse | null = await HttpUtil.post( httpOptions );
+			expect( response ).not.toBeNull();
+			expect( response ).toHaveProperty( `status` );
+			expect( response ).toHaveProperty( `statusText` );
+			expect( response ).toHaveProperty( `headers` );
+			expect( response ).toHaveProperty( `data` );
+			if ( response )
+			{
+				expect( response.status ).toBeGreaterThanOrEqual( 200 );
+				expect( response.status ).toBeLessThanOrEqual( 202 );
+
+				//	data: { id: 101 }
+				expect( response.data ).toHaveProperty( `id` );
+				expect( _.isNumber( response.data.id ) ).toBeTruthy();
+				expect( response.data.id ).toBeGreaterThan( 0 );
+			}
+		} );
+
+		// it( "should send HTTP Post request with proxy", async () =>
+		// {
+		// 	const httpOptions : HttpUtilOptions = {
+		// 		url : `https://jsonplaceholder.typicode.com/posts`,
+		//
+		// 		//	export https_proxy=http://127.0.0.1:6152;export http_proxy=http://127.0.0.1:6152;export all_proxy=socks5://127.0.0.1:6153
+		// 		proxy : {
+		// 			protocol: `http`,
+		// 			host: `127.0.0.1`,
+		// 			port: 6152,
+		// 		}
+		// 	};
+		// 	const response : HttpUtilResponse | null = await HttpUtil.post( httpOptions );
+		// 	expect( response ).not.toBeNull();
+		// 	expect( response ).toHaveProperty( `status` );
+		// 	expect( response ).toHaveProperty( `statusText` );
+		// 	expect( response ).toHaveProperty( `headers` );
+		// 	expect( response ).toHaveProperty( `data` );
+		// 	if ( response )
+		// 	{
+		// 		expect( response.status ).toBeGreaterThanOrEqual( 200 );
+		// 		expect( response.status ).toBeLessThanOrEqual( 202 );
+		//
+		// 		//	data: { id: 101 }
+		// 		expect( response.data ).toHaveProperty( `id` );
+		// 		expect( _.isNumber( response.data.id ) ).toBeTruthy();
+		// 		expect( response.data.id ).toBeGreaterThan( 0 );
+		// 	}
+		// } );
+	} );
+} );
